b633ea12bc43e947e61f639f55769cdcfa6e41c2
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2020-08-08 16:00:22 +0530

    Create MyAppletDemo.java

---
+++ applet_program/MyAppletDemo.java
@@ -0,0 +1,12 @@
+/* <applet code="MyAppletDemo" width="300" height="300"></applet>  */
+
+import java.applet.*;
+import java.awt.*;
+public class MyAppletDemo extends Applet
+{
+	
+	public void paint(Graphics g)
+	{
+		g.drawString("MacTech.com",100,100);
+	}	
+}
