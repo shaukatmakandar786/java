62d1f1f8641f68f039ce26514597ef7b4d6f0159
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2020-08-10 23:56:41 +0530

    Create Reverse_File.java

---
+++ file_program/Reverse_File.java
@@ -0,0 +1,53 @@
+//read data from a file convert into a reverse case and write in another file
+
+
+/*
+import java.io.*;
+class Reverse_File
+{
+	public static void main(String[] args)
+	{
+		File f1=new File("sample.txt");
+		if(f1.exists())
+		{
+		
+			try
+			{
+				FileInputStream fin=new FileInputStream("sample.txt");
+				FileOutputStream fout=new FileOutputStream("temp.txt");
+				
+				int c;
+				while((c=fin.read())!=-1)
+				{
+					
+					if(c>='A' && c<='Z')
+					{
+						fout.write((char)c+32);
+					}
+					else if(c>='a' && c<='z')
+					{
+						fout.write((char)c-32);
+					}
+					else
+					{	
+						fout.write((char)c);
+					}
+				}
+				
+				
+				fin.close();
+				fout.close();
+	
+			}
+			catch(IOException e)
+			{
+				System.out.println(e);
+			}
+		}
+		else
+		{
+			System.out.println("file not found");
+		}
+	}
+}
+*/
