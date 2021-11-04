a93d9c9ebe86f69a2f2933b2c62dd413d40a9121
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2021-11-05 01:58:38 +0530

    SuppressWarning  Anotation example

---
+++ annotations/02-SuppressAnnotationExample.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.*;
+class Bank
+{
+    @SuppressWarnings("unchecked")
+    public ArrayList<String> getCustomerDetails()
+    {
+        ArrayList a1=new ArrayList();
+        a1.add("aaa");
+        a1.add("bbb");
+        a1.add("ccc");
+        a1.add("ddd");
+
+        return a1;
+    }
+}
+
+public class SuppressAnnotationExample {
+
+    public static void main(String[] args) {
+
+        Bank b1=new Bank();
+        ArrayList a1=b1.getCustomerDetails() ;
+        System.out.println(a1);
+    }
+}
