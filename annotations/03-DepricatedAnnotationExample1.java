21d80a7ba44d201ee2e097c44e47e0db72b66326
MacTech9860 <39257815+shaukatmakandar786@users.noreply.github.com>
2021-11-05 09:21:13 +0530

    Create 03-DepricatedAnnotationExample1.java

---
+++ annotations/03-DepricatedAnnotationExample1.java
@@ -0,0 +1,16 @@
+//@Deprecated Annotation
+
+//It is a marker annotation. It indicates that a declaration is obsolete and has been replaced by a newer form.
+
+package com.company;
+
+import java.awt.*;
+public class DepricatedAnnotationExample1 {
+
+    public static void main(String[] args) {
+
+        Frame f=new Frame();
+        f.show();
+        //f.setVisible(true);
+    }
+}
