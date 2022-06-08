a34578b271ad2116a1a3c7e32f6332f0dec01ebd
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-06-08 16:48:31 +0530

    Create 04_ReverseString.java

---
+++ programs/04_ReverseString.java
@@ -0,0 +1,16 @@
+//Java program to print duplicate words ina String?
+
+public class SecondLargestNumber {
+
+    public static void main(String[] args) {
+
+        String  s1="this is java and java is great";
+
+        StringBuffer sb=new StringBuffer(s1);
+        sb.reverse();
+        System.out.println(sb);
+      
+    }
+}
+
+// taerg si avaj dna avaj si siht
