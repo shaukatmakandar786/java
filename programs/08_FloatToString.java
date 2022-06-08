7e925856e7ef021c22aa4313dec8975d01715d56
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-06-08 17:41:38 +0530

    Create 08_FloatToString.java

---
+++ programs/08_FloatToString.java
@@ -0,0 +1,11 @@
+public class FloatToString {
+
+    public static void main(String[] args) {
+
+        float a=3.14f;
+
+        String s=String.valueOf(a);
+        System.out.println(s);
+    }
+}
+// 3.14
