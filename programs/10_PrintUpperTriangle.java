7edba27017b083f05fef9c2c6ff15fd1223c74cf
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-06-08 17:51:49 +0530

    Create 10_PrintUpperTriangle.java

---
+++ programs/10_PrintUpperTriangle.java
@@ -0,0 +1,25 @@
+//print upper triangle
+public class PrintUpperTriangle {
+
+    public static void main(String[] args) {
+
+        for(int i=1;i<=5;i++)
+        {
+            for(int k=4;k>=i;k--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//      *
+//     * *
+//    * * *
+//   * * * *
+//  * * * * *
