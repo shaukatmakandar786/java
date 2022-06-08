b01e6e21bc0658acd0ac37e3ea50004e4ddf9047
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-06-08 18:02:10 +0530

    Create 13_ConvertFromObjectToString.java

---
+++ programs/13_ConvertFromObjectToString.java
@@ -0,0 +1,13 @@
+//convert from object to string
+
+public class convertObjectToString {
+    public static void main(String[] args) {
+
+        Integer i1=new Integer(10);
+        String s = i1.toString();
+
+        System.out.println(s);
+    }
+}
+
+// 10
