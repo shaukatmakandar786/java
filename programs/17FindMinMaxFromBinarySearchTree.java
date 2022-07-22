9eadd36045af1e60874b2c99597c2fd94cb28f50
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-07-22 15:10:10 +0530

    Update 17FindMinMaxFromBinarySearchTree.java

--- programs/17FindMinMaxFromBinarySearchTree.java
+++ programs/17FindMinMaxFromBinarySearchTree.java
@@ -72,7 +72,7 @@ public class BinaryTree {
         tree.root=new Node(10);
         tree.root.left=new Node(1);
         tree.root.right=new Node(50);
-        tree.root.left.left=new Node(8);
+        tree.root.left.right=new Node(8);
         tree.root.right.left=new Node(30);
         tree.root.right.right=new Node(54);
         tree.root.left.right=new Node(2);
