002dbdef8a0c2cdf953052d941271935c1e5252a
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2024-07-16 22:32:43 +0530

    Create SecondHighestNumber1.java

---
+++ Practice-06-2024/SecondHighestNumber1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+         int a[]={2,3,5,9,1,6,3,4,4,7};
+         Arrays.sort(a);
+         System.out.println(a[a.length-2]);
+    }
+}
+
+// 7
