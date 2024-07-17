cb110ff310787fa97919d1cfd0d7819a30c1522c
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2024-07-17 12:01:48 +0530

    Create Reverse1.java

---
+++ Practice-06-2024/Reverse1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        String str="This is java and java is secure language";
+        
+        StringBuffer sb=new StringBuffer(str);
+        System.out.println(sb.reverse());
+        
+    }
+}
+
+// egaugnal eruces si avaj dna avaj si sihT
